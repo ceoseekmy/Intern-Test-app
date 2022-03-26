f5baf5c5e97a64941875cbe445fc1f15051aa99a
atharva-seekmy <atharva@seekmy.health>
2022-03-26 15:31:25 +0530

    auth : login_email done

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: Number, required: true, unique: true },
 });
 
 const User = mongoose.model("User", userSchema);

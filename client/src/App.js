b4f7c281f1e0d62014f86d3c583990abc5a92d37
Harsh Mishra <harsh18mishra@gmail.com>
2022-04-01 22:20:25 +0530

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,88 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route,Navigate,} from "react-router-dom"; ///*/*/*/*//*/*/ exact ,render
-import ChatRoom from "./Pages/ChatRoom";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import Root from "./Pages/Root";
-import LoginWithOtp from "./Pages/LoginWithOtp";
-import io from "socket.io-client";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; ///*/*/*/*//*/*/ exact ,render
+import ChatRoom from './Pages/ChatRoom';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Root from './Pages/Root';
+import LoginWithOtp from './Pages/LoginWithOtp';
+import io from 'socket.io-client';
+import './App.css';
 
 function App() {
+    const [socket, setSocket] = useState(null);
+    const [cc_token, setcc_token] = useState(
+        localStorage.getItem('loginToken')
+    );
+    const setupSocket = () => {
+        const token = localStorage.getItem('loginToken');
+        setcc_token(localStorage.getItem('loginToken'));
+        if (token && !socket) {
+            const newSocket = io('http://localhost:4000', {
+                query: {
+                    token: localStorage.getItem('loginToken'),
+                },
+            });
+            newSocket.on('disconnect', () => {
+                setSocket(null);
+                setTimeout(setupSocket, 3000);
+            });
 
-  const [socket, setSocket] = useState(null);
-  const [cc_token,setcc_token]=useState(localStorage.getItem('loginToken'))
-  const setupSocket = () => {
-   const token=localStorage.getItem('loginToken');
-   setcc_token(localStorage.getItem('loginToken'))
-   if(token && !socket){
-     const newSocket=io("http://localhost:4000",{
-       query:{
-         token:localStorage.getItem('loginToken')
-       }
-     })
-     newSocket.on("disconnect", () => {
-      setSocket(null);
-      setTimeout(setupSocket, 3000);
-      
-    });
+            setSocket(newSocket);
+        } else {
+            return (
+                <Route
+                    path="/login"
+                    element={<Login setupSocket={setupSocket} />}
+                    exact
+                />
+            );
+        }
+    };
+    useEffect(() => {
+        setupSocket();
+        setcc_token(localStorage.getItem('loginToken'));
+        //eslint-disable-next-line
+    }, []);
 
-     setSocket(newSocket);
-   }
-   else{
-     
- return  <Route
- path="/login"
- element={<Login setupSocket={setupSocket} />}
- exact
-/>
-    }
-  };
-  useEffect(() => {
-    setupSocket();
-   setcc_token(localStorage.getItem('loginToken'))
-    //eslint-disable-next-line
-  }, []);
+    return (
+        <>
+            {/* navbar here */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Root />} exact />
 
-  return (
-    <BrowserRouter>
-      <Routes >
-      <Route path="/" element={<Root />} exact />
+                    <Route path="/register" element={<Register />} exact />
 
-        <Route path="/register" element={<Register />} exact />
+                    <Route
+                        path="/login"
+                        element={<Login setupSocket={setupSocket} />}
+                        exact
+                    />
+                    <Route
+                        path="/loginwithotp"
+                        element={<LoginWithOtp setupSocket={setupSocket} />}
+                        exact
+                    />
 
-        <Route
-          path="/login"
-          element={<Login setupSocket={setupSocket} />}
-          exact
-          
-        />
-        <Route path="/loginwithotp" element={<LoginWithOtp setupSocket={setupSocket} />} exact />
-      
-        <Route
-          path="/chatroom"
-          element={cc_token ?(socket? <ChatRoom socket={socket} />:setupSocket()): <Navigate to="/login"/>}
-          exact
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+                    <Route
+                        path="/chatroom"
+                        element={
+                            cc_token ? (
+                                socket ? (
+                                    <ChatRoom socket={socket} />
+                                ) : (
+                                    setupSocket()
+                                )
+                            ) : (
+                                <Navigate to="/login" />
+                            )
+                        }
+                        exact
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- client/src/Pages/Root.js
+++ client/src/Pages/Root.js
@@ -1,42 +1,89 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import background from '../assets/homepage.png';
 
 export default function Root() {
-  return (
-    <div className="card">
-    <div className="cardHeader">Let's Chat</div>
-    <div className="cardBody">
-      <div className="inputGroup">
-      <button type="submit">
-   
-    <Link to='/login'> Log in (user/Pass)</Link>
-    </button>
-    
-      </div>
-     
-     
-    </div>
-    <div className="cardBody">
-      <div className="inputGroup">
-      <button type="submit">
-  
-    <Link to='/loginwithotp'>  log in (Mobile)</Link>
-    </button>
-    
-      </div>
-     
-     
-    </div>
-    <div className="inputGroup">
-    <button type="submit" >
-    log in (Google)
-    </button>
-    </div>
-    
-    <button type="submit" >
+    return (
+        <div className="flex">
+            <div className="flex basis-1/2 h-screen justify-center items-center">
+                <div className="flex flex-col bg-fuchsia-300 w-2/3 rounded-lg justify-center items-center">
+                    <h2 className="text-2xl text-gray-900 font-semibold m-4 p-4 font-mono">
+                        Hello there!😀{' '}
+                    </h2>
+                    <button
+                        className="text-white text-xl bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 p-4 m-4 w-2/3"
+                        type="submit"
+                    >
+                        <Link to="/login"> Log in (user/Pass)</Link>
+                    </button>
+                    <button
+                        className="text-white text-xl bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 p-4 m-4 w-2/3"
+                        type="submit"
+                    >
+                        <Link to="/loginwithotp"> log in (Mobile)</Link>
+                    </button>
+                    <button
+                        type="button"
+                        class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-xl px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2 p-4 m-4 w-2/3"
+                    >
+                        <svg
+                            class="w-4 h-4 mr-2 ml-10"
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fab"
+                            data-icon="google"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 488 512"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                            ></path>
+                        </svg>
+                        Sign in with Google
+                    </button>
+                    {/* <button type="submit">log in (Google)</button> */}
+                    <button
+                        className="text-white text-xl bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 p-4 m-4 mb-8 w-2/3"
+                        type="submit"
+                    >
+                        <Link to="/register">Register</Link>
+                    </button>
+                </div>
+            </div>
+            <div className="flex bg-slate-400 h-screen basis-1/2">
+                <img className="w-full" src={background} alt="homepage icon" />
+            </div>
+        </div>
+    );
 
-    <Link to='/register'>Register</Link>
-    </button>
-  </div>
-  )
+    /*return (
+		
+        <div className="card">
+            <div className="cardHeader">Let's Chat</div>
+            <div className="cardBody">
+                <div className="inputGroup">
+                    <button type="submit">
+                        <Link to="/login"> Log in (user/Pass)</Link>
+                    </button>
+                </div>
+            </div>
+            <div className="cardBody">
+                <div className="inputGroup">
+                    <button type="submit">
+                        <Link to="/loginwithotp"> log in (Mobile)</Link>
+                    </button>
+                </div>
+            </div>
+            <div className="inputGroup">
+                <button type="submit">log in (Google)</button>
+            </div>
+
+            <button type="submit">
+                <Link to="/register">Register</Link>
+            </button>
+        </div>
+    );
+	*/
 }

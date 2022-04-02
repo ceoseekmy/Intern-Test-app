9d0871205272828d5ca140e0b34ea016c18f3422
atharva-seekmy <atharva@seekmy.health>
2022-04-02 16:14:16 +0530

    UI : finished

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-
+import background from "../assets/homepage.png";
 function Login(props) {
   const emailRef = React.createRef();
   const passwordRef = React.createRef();
@@ -28,32 +28,83 @@ function Login(props) {
   };
 
   return (
-    <div className="card">
-      <div className="cardHeader">Login</div>
-      <div className="cardBody">
-        <div className="inputGroup">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="abc@example.com"
-            ref={emailRef}
-          />
+    <div className="block md:flex">
+      <div className="flex basis-1/2 h-screen justify-center items-center">
+        <div className="flex flex-col bg-fuchsia-300 w-2/3 rounded-lg justify-center items-center">
+          <h2 className="text-2xl text-gray-900 font-semibold m-4 p-4 font-mono">
+            Login
+          </h2>
+          <div className="w-4/5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300"
+            >
+              Email:
+            </label>
+            <input
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="abc@example.com"
+              ref={emailRef}
+            />
+          </div>
+          <div className="w-4/5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300"
+            >
+              Password
+            </label>
+            <input
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your Password"
+              ref={passwordRef}
+            />
+          </div>
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-l w-4/5 px-5 py-2.5 text-center my-4"
+            onClick={loginUser}
+          >
+            Login
+          </button>
         </div>
-        <div className="inputGroup">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Your Password"
-            ref={passwordRef}
-          />
-        </div>
-        <button onClick={loginUser}>Login</button>
+      </div>
+      <div className="bg-slate-400 h-screen basis-1/2 hidden md:block">
+        <img className="w-full h-full" src={background} alt="homepage icon" />
       </div>
     </div>
+
+    // <div className="card">
+    //   <div className="cardHeader">Login</div>
+    //   <div className="cardBody">
+    //     <div className="inputGroup">
+    //       <label htmlFor="email">Email</label>
+    //       <input
+    //         type="email"
+    //         name="email"
+    //         id="email"
+    //         placeholder="abc@example.com"
+    //         ref={emailRef}
+    //       />
+    //     </div>
+    //     <div className="inputGroup">
+    //       <label htmlFor="password">Password</label>
+    //       <input
+    //         type="password"
+    //         name="password"
+    //         id="password"
+    //         placeholder="Your Password"
+    //         ref={passwordRef}
+    //       />
+    //     </div>
+    //     <button onClick={loginUser}>Login</button>
+    //   </div>
+    // </div>
   );
 }
 
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,14 +1,13 @@
 import React from "react";
 import axios from "axios";
-import { Link,useNavigate } from "react-router-dom";
-
-
+import { Link, useNavigate } from "react-router-dom";
+import background from "../assets/homepage.png";
 function Register() {
   const nameRef = React.createRef();
   const emailRef = React.createRef();
   const passwordRef = React.createRef();
   const phoneRef = React.createRef();
-const navigate=useNavigate();
+  const navigate = useNavigate();
   const registerUser = () => {
     const name = nameRef.current.value;
     const email = emailRef.current.value;
@@ -23,7 +22,7 @@ const navigate=useNavigate();
       })
       .then((response) => {
         console.log("lets go to login");
-             navigate("/");
+        navigate("/");
       })
       .catch((err) => {
         console.log(err);
@@ -31,52 +30,136 @@ const navigate=useNavigate();
   };
 
   return (
-    <div className="card">
-      <div className="cardHeader">Registration</div>
-      <div className="cardBody">
-        <div className="inputGroup">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Your Name"
-            ref={nameRef}
-          />
+    <div className="block md:flex">
+      <div className="flex basis-1/2 h-screen justify-center items-center">
+        <div className="flex flex-col bg-fuchsia-300 w-11/12 md:w-2/3 rounded-lg justify-center items-center">
+          <h2 className="text-2xl text-gray-900 font-semibold m-4 p-4 font-mono">
+            Sign up
+          </h2>
+          <div className="w-4/5">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300"
+            >
+              Name:
+            </label>
+            <input
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Your Name"
+              ref={nameRef}
+            />
+          </div>
+          <div className="w-4/5">
+            <label
+              className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="email"
+            >
+              Email:
+            </label>
+            <input
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="abc@example.com"
+              ref={emailRef}
+            />
+          </div>
+          <div className="w-4/5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300"
+            >
+              Password:
+            </label>
+            <input
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your Password"
+              ref={passwordRef}
+            />
+          </div>
+          <div className="w-4/5">
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300"
+            >
+              Mobile no:
+            </label>
+            <input
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+              type="text"
+              name="phone"
+              id="phone"
+              placeholder="Your mobile no."
+              ref={phoneRef}
+            />
+          </div>
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-l w-4/5 px-5 py-2.5 text-center my-4"
+            type="submit"
+            onClick={registerUser}
+          >
+            Register
+          </button>
         </div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="abc@example.com"
-          ref={emailRef}
-        />
       </div>
-      <div className="inputGroup">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Your Password"
-          ref={passwordRef}
-        />
+      <div className="bg-slate-400 h-screen basis-1/2 hidden md:block">
+        <img className="w-full h-full" src={background} alt="homepage icon" />
       </div>
-      <div className="inputGroup">
-        <label>Mobile no.</label>
-        <input
-          type="text"
-          name="phone"
-          id="phone"
-          placeholder="Your mobile no."
-          ref={phoneRef}
-        />
-      </div>
-      <button type="submit" onClick={registerUser}>
-      Register
-      </button>
     </div>
+
+    // <div className="card">
+    //   <div className="cardHeader">Registration</div>
+    //   <div className="cardBody">
+    //     <div className="inputGroup">
+    //       <label htmlFor="name">Name</label>
+    //       <input
+    //         type="text"
+    //         name="name"
+    //         id="name"
+    //         placeholder="Your Name"
+    //         ref={nameRef}
+    //       />
+    //     </div>
+    //     <label htmlFor="email">Email</label>
+    //     <input
+    //       type="email"
+    //       name="email"
+    //       id="email"
+    //       placeholder="abc@example.com"
+    //       ref={emailRef}
+    //     />
+    //   </div>
+    //   <div className="inputGroup">
+    //     <label htmlFor="password">Password</label>
+    //     <input
+    //       type="password"
+    //       name="password"
+    //       id="password"
+    //       placeholder="Your Password"
+    //       ref={passwordRef}
+    //     />
+    //   </div>
+    //   <div className="inputGroup">
+    //     <label>Mobile no.</label>
+    //     <input
+    //       type="text"
+    //       name="phone"
+    //       id="phone"
+    //       placeholder="Your mobile no."
+    //       ref={phoneRef}
+    //     />
+    //   </div>
+    //   <button type="submit" onClick={registerUser}>
+    //   Register
+    //   </button>
+    // </div>
   );
 }
 
